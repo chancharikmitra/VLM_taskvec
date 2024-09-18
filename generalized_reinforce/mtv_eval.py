c4ddf5e966e5ff1a625d4bf6373844f094fb2bd3
Brandon3964 <brrrandonhuang@gmail.com>
2024-09-18 23:52:12 +0000

    added mtv_eval

--- generalized_reinforce/mtv_eval.py
+++ generalized_reinforce/mtv_eval.py
@@ -1,3 +1,9 @@
+# from llava.model.builder import load_pretrained_model
+# from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
+# from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
+# from llava.conversation import conv_templates
+
+
 from mtv_utils import *
 from models import *
 from preprocess import *
@@ -11,6 +17,8 @@ from transformers.utils import logging
 import sys
 logging.set_verbosity_error() 
 
+
+
 def eval_reinforce(args):
 
     train_dataset = open_data(args.data_name, args.train_path)
@@ -18,87 +26,110 @@ def eval_reinforce(args):
 
 
     activation_data = train_dataset
-    reinforce_data = random.sample(train_dataset, 100)
+    # reinforce_data = random.sample(train_dataset, 100)
+    reinforce_data = train_dataset
     eval_data = val_dataset[:50]
 
 
     ##Load the model
     model_helper = load_model(args.model_name, args.data_name, args.meta_mtv)
 
-
     ##Mean activation of some in-context input
-
     if args.cur_mode != "clean":
-        if model_helper.meta_mtv:
-            mean_activations = meta_last_mean_head_activations(activation_data, model_helper, N_TRIALS = args.num_example, shot=args.num_shot)
-        else:
-            mean_activations = get_last_mean_head_activations(activation_data, model_helper, N_TRIALS = args.num_example, shot=args.num_shot)
-        torch.save(mean_activations, args.activation_path)
+        # if model_helper.meta_mtv:
+        #     mean_activations = meta_last_mean_head_activations(activation_data, model_helper, N_TRIALS = args.num_example, shot=args.num_shot)
+        # else:
+        #     mean_activations = get_last_mean_head_activations(activation_data, model_helper, N_TRIALS = args.num_example, shot=args.num_shot)
+
+        #torch.save(mean_activations, args.activation_path)
         mean_activations = torch.load(args.activation_path)
 
-        # # # ##Examples from the test set is used to visualize the validation loss
-        bernoullis = reinforce(mean_activations, model_helper, reinforce_data, eval_data)
-        torch.save(bernoullis, args.bernoullis_path)
+        # # ##Examples from the test set is used to visualize the validation loss
+        # bernoullis = reinforce(mean_activations, model_helper, reinforce_data, eval_data)
+        # torch.save(bernoullis, args.bernoullis_path)
         bernoullis = torch.load(args.bernoullis_path)
 
+        
 
         ###Sample from the trained distribution and identify the intervention locations
         sigmoid_tensor = torch.stack([torch.sigmoid(bernoulli).clamp(min=0, max=1) for bernoulli in bernoullis])
         ###Thresholding heads with low probability from being sampled
-        # sigmoid_tensor = torch.nn.functional.threshold(sigmoid_tensor, 0.9, 0)
+        #sigmoid_tensor = torch.nn.functional.threshold(sigmoid_tensor, 0.9, 0)
+        
         prob_dist = torch.distributions.Bernoulli(sigmoid_tensor)
         sampled = prob_dist.sample()
+
+        
         intervention_locations = reinforce_intervention_location(sampled)
-        print(len(intervention_locations)) 
+        print(intervention_locations)
+        print(len(intervention_locations))
+        
+    # else:
+    #     mean_activations = None
+    #     intervention_locations = None
 
-    else:
-        mean_activations = None
-        intervention_locations = None
-
-    clean_answers = []
-    interv_answers = []
-    clean_count, interv_count = 0, 0
-
-
-    for item in tqdm(val_dataset):
+    # clean_answers = []
+    # interv_answers = []
+    # clean_count, interv_count = 0, 0
 
 
-        text, image_list, target_out, question_id = model_helper.format_func(train_dataset, item, num_shot=args.eval_num_shot)
-
-        new_input = model_helper.insert_image(text, image_list)
+    # for item in tqdm(val_dataset):
 
 
-        clean_out, interv_out = fv_intervention_natural_text(new_input, model_helper, max_new_tokens=args.max_token, return_item=args.cur_mode, intervention_locations=intervention_locations, avg_activations=mean_activations)
+    #     text, image_list, target_out, question_id = model_helper.format_func(train_dataset, item, num_shot=args.eval_num_shot)
 
-        if model_helper.cur_dataset == "info":
-            clean_count += int(clean_out.split(".")[0].split("\n")[0].strip().lower() in question_id)
-            interv_count += int(interv_out.split(".")[0].split("\n")[0].strip().lower() in question_id)
-            continue
+    #     new_input = model_helper.insert_image(text, image_list)
 
-        # sys.flush()
 
-        interv_answers.append({"answer":interv_out.split(".")[0].split("\n")[0].strip(), "question_id":question_id})
-        clean_answers.append({"answer":clean_out.split(".")[0].split("\n")[0].strip(), "question_id":question_id})
+    #     clean_out, interv_out = fv_intervention_natural_text(new_input, model_helper, max_new_tokens=args.max_token, return_item=args.cur_mode, intervention_locations=intervention_locations, avg_activations=mean_activations)
+    #     print(interv_out, target_out)
 
-        clean_count += int(clean_out.split(".")[0].split("\n")[0].strip().lower() == target_out.lower())
-        interv_count += int(interv_out.split(".")[0].split("\n")[0].strip().lower() == target_out.lower())
+    #     ###For llava-oa. DELETE ME
+    #     clean_out = clean_out.replace("The answer is", "").strip()
+    #     interv_out = interv_out.replace("The answer is", "").strip()
 
-    if args.is_eval:
 
-        if args.cur_mode == "interv" or args.cur_mode == "both":
+    #     if model_helper.cur_dataset == "info":
+            
+    #         clean_count += int(clean_out.split(".")[0].split("\n")[0].strip().lower() in question_id)
+    #         interv_count += int(interv_out.split(".")[0].split("\n")[0].strip().lower() in question_id)
+    #         continue
+    #     if model_helper.cur_dataset == "ocr":
+    #         item_type = item["type"]
+    #         clean_count += eval_ocr(target_out, clean_out, item_type)
+    #         interv_count += eval_ocr(target_out, interv_out, item_type)
+    #         continue
 
-            if args.data_name == "flower" or args.data_name =="cub" or args.data_name == "ai2d" or args.data_name == "info" or args.data_name == "food" or args.data_name == "dtd":
-                print(f"Intervention Score:{interv_count/len(val_dataset)}")
-            else:
-                print(f"{args.data_name}_{args.experiment_name} Intervention Score:")
-                eval_vqa(f"{args.data_name}_val", args.result_folder + f"{args.experiment_name}_interv.json", interv_answers)
+    #     # sys.flush()
 
-        if args.cur_mode == "clean" or args.cur_mode == "both":
-            if args.data_name == "flower" or args.data_name =="cub" or args.data_name == "ai2d" or args.data_name == "info" or args.data_name == "food" or args.data_name == "dtd":
-                print(f"Clean Score:{clean_count/len(val_dataset)}")
-            else:
-                print(f"{args.data_name}_{args.experiment_name} Clean Score:")
-                eval_vqa(f"{args.data_name}_val", args.result_folder + f"{args.experiment_name}_clean.json", clean_answers)
+    #     if model_helper.cur_dataset == "vizwiz" and args.model_name == "Qwen":
+    #         interv_answers.append({"answer":interv_out, "question_id":question_id})
+    #         clean_answers.append({"answer":clean_out, "question_id":question_id})
+    #     else:
+    #         interv_answers.append({"answer":interv_out.split(".")[0].split("\n")[0].strip(), "question_id":question_id})
+    #         clean_answers.append({"answer":clean_out.split(".")[0].split("\n")[0].strip(), "question_id":question_id})
+
+
+
+    #     clean_count += int(clean_out.split(".")[0].split("\n")[0].strip().lower() == target_out.lower())
+    #     interv_count += int(interv_out.split(".")[0].split("\n")[0].strip().lower() == target_out.lower())
+
+    # if args.is_eval:
+
+    #     if args.cur_mode == "interv" or args.cur_mode == "both":
+
+    #         if args.data_name == "flower" or args.data_name =="cub" or args.data_name == "ai2d" or args.data_name == "info" or args.data_name == "food" or args.data_name == "dtd" or args.data_name == "hateful" or args.data_name == "path" or args.data_name == "ocr" or args.data_name == "sst2":
+    #             print(f"Intervention Score:{interv_count/len(val_dataset)}")
+    #         else:
+    #             print(f"{args.data_name}_{args.experiment_name} Intervention Score:")
+    #             eval_vqa(f"{args.data_name}_val", args.result_folder + f"{args.experiment_name}_interv.json", interv_answers)
+
+    #     if args.cur_mode == "clean" or args.cur_mode == "both":
+    #         if args.data_name == "flower" or args.data_name =="cub" or args.data_name == "ai2d" or args.data_name == "info" or args.data_name == "food" or args.data_name == "dtd" or args.data_name == "hateful" or args.data_name == "path" or args.data_name == "ocr" or args.data_name == "sst2":
+    #             print(f"Clean Score:{clean_count/len(val_dataset)}")
+    #         else:
+    #             print(f"{args.data_name}_{args.experiment_name} Clean Score:")
+    #             eval_vqa(f"{args.data_name}_val", args.result_folder + f"{args.experiment_name}_clean.json", clean_answers)
 
 
 if __name__ == "__main__":
